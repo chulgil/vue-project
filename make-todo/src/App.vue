ec59432ef0908113f53344e08356dad55f2fa794
codenavi <vncodenavi@gmail.com>
2018-11-15 14:18:34 -0600

    add components

---
+++ make-todo/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+ import TodoHeader from './components/TodoHeader.vue'
+ import TodoInput from './components/TodoInput.vue'
+ import TodoList from './components/TodoList.vue'
+ import TodoFooter from './components/TodoFooter.vue'
+
+//es5 기법
+/*
+var my_cmp = {
+  template: '<div>my component</div>'
+};
+
+
+new Vue({
+  el:'',
+  components:{
+    'my-cmp': my_cmp // -하이픈 케밥 기법
+  }
+});
+*/
+
+//es6 기법
+export default{
+  // 컴포넌트 태그명 : 컴포넌트 내용
+  components : {
+    'TodoHeader': TodoHeader,
+    'TodoList' : TodoList,
+    'TodoInput' : TodoInput,
+    'TodoFooter' : TodoFooter
+    }
+}
+
+</script>
+
+<style>
+
+</style>
