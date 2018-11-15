6b3e7c88f139f9d592cc7f877a2a04ff2e30696e
codenavi <vncodenavi@gmail.com>
2018-11-15 15:25:02 -0600

    styling todo list

--- make-todo/src/components/TodoList.vue
+++ make-todo/src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
         {{ todoItem }}
+        <span class="removeBtn">
+          <i class="fas fa-trash-alt"></i>
+        </span>
       </li>
     </ul>
   </div>
@@ -15,7 +18,11 @@ export default {
       todoItems: []
     }
   },
+  methods: {
+    removeTodo: function() {
 
+    }
+  },
   created: function() {
     if(localStorage.length > 0) {
       for(var i = 0; i < localStorage.length ; i ++) {
@@ -30,6 +37,48 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.shadow{
+  box-shadow: 5px 10px 10px rgba(0,0,0,0.03)
+}
+
+.removeBtn{
+  margin-left:auto;
+  color:#de4343;
+}
+
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top:0;
+  text-align: left;
+}
+
+li{
+  display: flex;
+  min-height: 50px;
+  height:50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.checkBtn{
+  line-height: 45px;
+  color:#62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted{
+  color:#b3adad;
+}
+
+.textCompleted{
+  text-decoration: line-through;
+  color:#b3adad;
+}
 
 </style>
