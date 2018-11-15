9b559944f6767acbe12f9e74a2c5473261c6b0fb
codenavi <vncodenavi@gmail.com>
2018-11-15 14:52:44 -0600

    add todo input box

--- make-todo/src/components/TodoInput.vue
+++ make-todo/src/components/TodoInput.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    input
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">Add</button>
+
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem)
+      // 로컬 스토리지에 저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = '';
+    }
+  }
 
 }
 </script>
