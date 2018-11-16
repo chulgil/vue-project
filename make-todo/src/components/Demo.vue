7dc6b42b2a6bf887c260782bbc058710ef3c3861
codenavi <vncodenavi@gmail.com>
2018-11-16 13:50:43 -0600

    add helper function in Demo.vue

---
+++ make-todo/src/components/Demo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="root">
+    <p>{{ originalPrice }}</p>
+    <p>{{ doublePrice }}</p>
+    <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['originalPrice', 'doublePrice', 'triplePrice']),
+    // originalPrice() {
+    //   return this.$store.gettres.originalPrice;
+    // },
+    // doublePrice() {
+    //   return this.$store.gettres.doublePrice;
+    // },
+    // triplePrice() {
+    //   return this.$store.gettres.triplePrice;
+    // },    
+  }
+}
+</script>
+
+<style>
+
+</style>
